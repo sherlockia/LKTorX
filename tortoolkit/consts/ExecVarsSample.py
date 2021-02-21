e8ac3f71f7495038c04f29f6da381a8ee50e0667
sherlockia <73867527+sherlockia@users.noreply.github.com>
2021-02-21 21:37:01 +0530

    Update ExecVarsSample.py

--- tortoolkit/consts/ExecVarsSample.py
+++ tortoolkit/consts/ExecVarsSample.py
@@ -15,7 +15,7 @@ except:
         SERVPORT = 80
 
         # ALLOWED USERS [ids of user or supergroup] seperate by commas
-        ALD_USR = [327413172,-498275457]
+        ALD_USR = [327413172, -498275457]
         
         # Google Drive Index Link should include the base dir also See readme for more info
         GD_INDEX_URL = False
